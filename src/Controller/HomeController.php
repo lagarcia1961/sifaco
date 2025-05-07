bb000c140877705a0732bef8569dfd3ed4753db3
unknown <lagarcia@ambiente.gob.ar>
2025-05-07 17:36:29 -0300

    Inicio del proyecto Symfony - SIFACO

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+
+#[IsGranted('ROLE_USER')]
+class HomeController extends AbstractController
+{
+    #[Route('/', name: 'app_home_front')]
+    public function index(): Response
+    {
+        return $this->render('home/index.html.twig');
+    }
+}
+
